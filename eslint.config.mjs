844c4493425303a4b3d5d5fd3dd37d2bc79006e5
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-04-09 07:23:59 +0530

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,5 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [{ languageOptions: { globals: globals.node } }, pluginJs.configs.recommended, prettierConfig];
