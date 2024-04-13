cf4cfc18da8ae830ca46de7b23988e052b2e962c
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-04-13 11:06:56 +0530

    feat: add password and token methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema(
   {
@@ -40,10 +42,39 @@ const userSchema = new Schema(
         ref: 'Video',
       },
     ],
+    refreshToken: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   },
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 15);
+  next();
+});
+
+userSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    { _id: this._id, fullName: this.fullName, email: this.email, userName: this.userName },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
 
 export const User = mongoose.model('User', userSchema);
