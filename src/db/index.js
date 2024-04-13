539e5af0f69bc0532df212e2753f491447460423
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2024-04-13 08:22:47 +0530

    feat: configure basic express app

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from '../constants.js';
 const connectDB = async () => {
   try {
     const dbInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`MongoDB Connected on HOST: ${dbInstance.connection.host}`);
+    console.log(`\nMongoDB Connected on HOST: ${dbInstance.connection.host}`);
   } catch (err) {
     console.error('MongoDB connetion failed', err);
     process.exit(1);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config({ path: './.env' });
 
+const PORT = process.env.PORT || 8000;
+
 connectDB()
   .then(() => {
-    console.log('Database connected');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.error(err.message);
+    console.error(err);
   });
